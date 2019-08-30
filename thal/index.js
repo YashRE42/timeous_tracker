0756324b02e0bd828ddfa3f78b29bf13a716f474
YashRE42 <YashRE42@github.com>
2019-08-31 03:56:54 +0530

    scrapper: attempt to open sidebar.

--- thal/index.js
+++ thal/index.js
@@ -32,8 +32,9 @@ const browser = await puppeteer.launch({
   await page.waitForNavigation();
   console.log("Hey");
   const menu = "#gb > div.gb_Sd.gb_9d.gb_1d.gb_0d > div.gb_Zc.gb_5c.gb_6c > div:nth-child(1)";
-  await page.click(menu);
-  await page.click("#gb > div.gb_de > div > div.gb_Lc > div > c-wiz > div > div > nav > a:nth-child(1)");
+  await page.$eval("#gb > div.gb_Sd.gb_9d.gb_1d.gb_0d > div.gb_Zc.gb_5c.gb_6c > div:nth-child(1)")
+  // await page.click("#gb > div.gb_Sd.gb_9d.gb_1d.gb_0d > div.gb_Zc.gb_5c.gb_6c > div:nth-child(1)");
+  await page.click("#gb > div.gb_de > div > div.gb_Lc > div > c-wiz > div > div > nav > a:nth-child(1)", a => {a.click()});
   const selector = ".KXhB0c.YYajNd";
   // const results = await page.$$(selector);
   // console.log(results);
